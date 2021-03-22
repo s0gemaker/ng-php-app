c3f8df1bc77f9d5d9bf481d8c427b93b1e953a1c
s0gemaker <s0gemaker@gmail.com>
2021-03-22 04:39:58 -0400

    sidenav portfolio done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PortfolioPageComponent } from './portfolio-page/portfolio-page.componen
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
@@ -35,7 +36,8 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
